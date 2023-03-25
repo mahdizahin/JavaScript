7b445a06765abfb274b9a8d3d44a49920dc3eda1
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-03-25 23:37:47 +0600

    Add files via upload

---
+++ #Short_Project/script.js
@@ -0,0 +1,18 @@
+/*console.log("Hey mz")
+
+ alert("Hey Mahdi Zahin");
+
+ document.write("uraaa");*/
+
+ let input= prompt("Enter value 1 : ");
+ let input2= prompt("Enter value for 2 : ");
+ //console.log(input)
+input= parseInt(input);
+input2= parseInt(input2);
+
+var sum=input+input2;
+
+ document.write(input+ " <br> ");
+ document.write(input2);
+
+ document.write(" <br> Sum is : "+sum);
