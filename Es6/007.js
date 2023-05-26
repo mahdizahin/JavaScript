2606a92211511dd018ec3af760d83067c3d9b3af
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-05-26 21:32:12 +0600

    Add files via upload

---
+++ Es6/007.js
@@ -0,0 +1,11 @@
+var c=[1,2,3,4,5,6];
+//of
+for(x of c){
+    console.log(x);
+}
+
+//in
+
+for(y in c){
+    console.log(y);
+}
---
+++ Es6/for 2.js
@@ -0,0 +1,5 @@
+var a=[7,8,9];
+
+for(obj of a){
+    console.log(obj)
+}
