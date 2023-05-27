c8656af8ee1e2d268742ec1a544da53b4f5a3f4d
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-05-27 22:34:55 +0600

    Add files via upload

---
+++ 007 Function/Date.js
@@ -0,0 +1,6 @@
+var date= new Date();
+
+console.log(date);
+console.log(date.toTimeString());
+console.log("For Local time : "+date.toLocaleTimeString());
+console.log("For Month "+ date.getMonth());
