c448faf78510acc681378e8cd7be8fb4a4683437
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-05-29 12:45:05 +0600

    Add files via upload

---
+++ Es6/Destructuring/006 obj.js
@@ -0,0 +1,24 @@
+obj = {
+
+    class:7,
+    roll:"2017-3",
+    name:"Toxic",
+    CG:"3.00",
+}
+
+console.log(obj.class,obj.roll,obj.name,obj.CG);
+console.log("----------------------");
+
+//start
+
+const anything = {
+
+    cls:"13.2",
+    rl:"2023-1",
+    nm:"Toxic",
+    CGPA:"3.00",
+}
+
+const {cls,rl,nm,CGPA} = anything;
+
+ console.log(cls);
