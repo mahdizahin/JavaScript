4acb4f3e88153513131a5c251e37a85e727bb9dc
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-05-29 12:44:24 +0600

    Add files via upload

---
+++ Es6/Destructuring/001 array.js
@@ -0,0 +1,6 @@
+arr=[5,4,3,2,1];
+
+for(i=0;i<5;i++){
+    console.log(`Number at index [${i}] = ${arr[i]}`);
+    //console.log(arr[i]);
+}
---
+++ Es6/Destructuring/007.js
@@ -0,0 +1,10 @@
+const objectx= {
+
+    cls:"13.2",
+    rl:"2023-1",
+    nm:"Toxic",
+    CGPA:"3.00",
+}
+ const {cls,rl,nm,CGPA} = objectx;
+console.log("Information for student 1 ")
+ console.log(cls,rl,CGPA);
