56341ead0f22aa1dbdf9e9eb606e8862ec0c2aa4
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2025-01-03 10:15:11 +0600

    Create 005 function in object2.js

---
+++ 008 Object/005 function in object2.js
@@ -0,0 +1,13 @@
+let empire={
+    empireName : "Mughal",
+    timeline : "Undefined",
+    ruller : function(){
+        console.log("r1")
+    },
+    army:function(){
+        this.ruller();
+        console.log("Capbility");
+    }
+}
+
+empire.army();
