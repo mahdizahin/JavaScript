d456d72ab8067a963d7c8067d24c017783c5a8d5
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2025-01-04 12:22:35 +0600

    Add files via upload

---
+++ Problem Solving/4 to 10.js
@@ -0,0 +1,2 @@
+const ludo = Math.floor(Math.random() * (10 - 4 + 1)) + 4;
+console.log("For Ludo (4-10):", ludo);
---
+++ Problem Solving/alphabeticallySort.js
@@ -0,0 +1,3 @@
+const names=["Nion","Zion","Musa","Junayed","Heron","Tawhid","Jolani","Putin"];
+
+console.log(names.sort());
