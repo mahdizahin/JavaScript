2c38e026bfc4dc2e74142097bd01cbba01c5deb8
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2025-01-17 00:26:21 +0600

    Update basic.js

---
+++ 032 class/basic.js
@@ -0,0 +1,25 @@
+class asixawt{
+
+    constructor(volume,isotopNumber){
+        this.volume=volume;
+        this.isotopNumber=isotopNumber;
+    }
+
+
+    display(){
+        console.log(this.volume);
+        console.log(this.isotopNumber);
+        
+    }
+
+    // constructor(a,b,c,d){
+    //     console.log(a,b,c,d);
+    // }
+}
+
+const prantoSpecializedHospital = new asixawt("0324839","KXXT865");
+
+prantoSpecializedHospital.display();
+
+//const ktx= new asixawt(5,6,8,3354);
+
