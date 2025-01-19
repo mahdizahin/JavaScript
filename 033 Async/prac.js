044bed13055774cb8458ce3f9f6ab03b5832b68c
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2025-01-19 21:15:25 +0600

    Create prac.js

---
+++ 033 Async/prac.js
@@ -0,0 +1,8 @@
+setTimerOut(function(){
+       console.log("Blink after 3 second");         
+           
+},3000);
+setTimerOut(function(){
+       console.log("Blink after 4 second");         
+           
+},4000);
